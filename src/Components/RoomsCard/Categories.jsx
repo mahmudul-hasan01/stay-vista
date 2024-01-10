b74109a9b104565c17de851a91d69e025898ec91
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-01-10 11:22:14 +0600

    add active category functionlity

--- src/Components/RoomsCard/Categories.jsx
+++ src/Components/RoomsCard/Categories.jsx
@@ -1,13 +1,16 @@
 import Container from "../Container/Container";
 import { categories } from "./CategoriesData";
 import CategoryBox from "./CategoryBox";
+import { useSearchParams } from "react-router-dom";
 
 const Categories = () => {
+    const [params,setParams] = useSearchParams()
+    const categorys = params.get('category')
     return (
         <Container>
             <div className="py-4 flex items-center justify-between overflow-x-auto">
                 {
-                categories.map((category,index)=> <CategoryBox key={index} label={category.label} icon={category?.icon}></CategoryBox>)
+                categories.map((category,index)=> <CategoryBox key={index} label={category.label} icon={category?.icon} selected={categorys === category?.label}></CategoryBox>)
                 }
             </div>
         </Container>
